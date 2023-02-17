7ea01face608d1222f90506a161dbd2a1893dd5f
Eternity Hacker <eternity@hotlookbook.com>
2023-02-17 12:58:22 -0800

    imported the insertGenres moduel and invoked it (called it)

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+import insertGenres from "./modules/insertGenres.js";
 import navigateGenres from "./modules/navigateGenres.js";
 import genrePopupForm from "./modules/genrePopupForm.js";
 
-
+insertGenres()
 navigateGenres()
 genrePopupForm()
- 
+
+
      
   
       
