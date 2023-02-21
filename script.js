9accc90eed343d01d14f73921cbbdb0c4723efa6
Eternity Hacker <eternity@hotlookbook.com>
2023-02-21 15:04:24 -0800

    changed input syntax for insertGenres from default to destructuring

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import insertGenres from "./modules/insertGenres.js";
+import { insertGenres } from "./modules/insertGenres.js";
 import navigateGenres from "./modules/navigateGenres.js";
 import genrePopupForm from "./modules/genrePopupForm.js";
 
