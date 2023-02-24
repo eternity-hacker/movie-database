8d1ad05acf762cbae5fdd0ba2049ed311882314e
Eternity Hacker <eternity@hotlookbook.com>
2023-02-23 16:51:55 -0800

    added genreForm selectors to window Object

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 import { insertGenres } from "./modules/insertGenres.js";
 import navigateGenres from "./modules/navigateGenres.js";
 import genrePopupForm from "./modules/genrePopupForm.js";
+import { genreForm } from "./modules/selectors.js"
+
+//genreForm is an object therefore in order to use it in the console I had to attach it to the window Object seen below.
+window.genreForm = genreForm
 
 insertGenres()
 navigateGenres()
 genrePopupForm()
 
 
-     
-  
-      
+
+
+
+
 
 
 
