b491bc617cc22dc7b4682b0666c37b5a826952ad
Eternity Hacker <eternity@hotlookbook.com>
2023-02-28 15:04:54 -0800

    detailed what each function call does

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ import { genreForm } from "./modules/selectors.js"
 //genreForm is an object therefore in order to use it in the console I had to attach it to the window Object seen below.
 window.genreForm = genreForm
 
+//1. populates supported genres to the home page select.  
+//2. populated supported genres to the popup forms select
 insertGenres()
+//allows the users to navigate different genres and their associated movies
 navigateGenres()
+//allows users to add a new movie to the database
 genrePopupForm()
 
 
